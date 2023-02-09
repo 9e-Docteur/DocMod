283bd89d8251da3e57c9b644b775e87e7cf5aef5
Leaziurd <guekbaile@gmail.com>
2023-02-09 21:46:17 +0100

    added some item stuff [don't] kill me plz

---
+++ src/main/java/fr/ninedocteur/docmod/common/item/StaffItem.java
@@ -0,0 +1,18 @@
+package fr.ninedocteur.docmod.common.item;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class StaffItem extends SwordItem {
+    public StaffItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.setOnFireFor(5);
+        return super.postHit(stack, target, attacker);
+    }
+}
